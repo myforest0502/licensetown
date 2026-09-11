fc4b7e6298be387937d781f2b48c0fced9702435
myforest0502 <licensetown.support@gmail.com>
2026-09-11 23:20:34 +0900

    refactor: point Takken config at common qualification contract

--- qualifications/takken/config.py
+++ qualifications/takken/config.py
@@ -1,5 +1,5 @@
 """Define Takken without connecting a bank, session, or database."""
 
-from ..base import QualificationConfig
+from ..common.base import QualificationConfig
 
 TAKKEN = QualificationConfig(qualification_id="takken", display_name="宅地建物取引士")
