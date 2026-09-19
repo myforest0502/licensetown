fd3557af2f2c5838041aabda6b22e8805a750784
myforest0502 <licensetown.support@gmail.com>
2026-09-19 20:51:16 +0900

    refactor: remove web session root alias

--- durable_web_learning_session.py
+++
@@ -1,5 +0,0 @@
-"""Compatibility alias for licensetown.common.durable_web_learning_session."""
-
-import sys
-from licensetown.common import durable_web_learning_session as _implementation
-sys.modules[__name__] = _implementation
